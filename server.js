83b7c5891a67ca61c16ad6b0a85207c1dc8b7edb
Glitch (nikonbott) <none>
2020-03-03 04:48:17 +0000

    🏓👨‍❤️‍💋‍👨 Checkpoint ./server.js:4207132/2276

--- server.js
+++ server.js
@@ -2405,5 +2405,12 @@ client.on('voiceStateUpdate', (voiceOld, voiceNew) => {
     }
 });
 
+client.on('message', message => {
+    if (message.content.toLowerCase().startsWith(prefix + `top-servers`)) {
+
+        const top = client.guilds.sort((a, b) => a.memberCount - b.memberCount).array().reverse()
+        message.channel.send(`**⇏ Top 25 Servers: **\n1. **${top[0].name}**: ${top[0].memberCount} \n2. **${top[1].name}**: ${top[1].memberCount}.\n3. **${top[2].name}**: ${top[2].memberCount}.\n4. **${top[3].name}**: ${top[3].memberCount}.\n5. **${top[4].name}**: ${top[4].memberCount}.\n6. **${top[5].name}**: ${top[5].memberCount}.\n7. **${top[6].name}**: ${top[6].memberCount}.\n8. **${top[7].name}**: ${top[7].memberCount}.\n9. **${top[8].name}**: ${top[8].memberCount}.\n10. **${top[9].name}**: ${top[9].memberCount} .`)
+    }
+});
 
 client.login(process.env.BOT_TOKEN)
